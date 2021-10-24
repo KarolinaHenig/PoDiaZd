093a4c540a7a53105bca96b2b0738456d26c24c6
unknown <karolina.henig@gmail.com>
2021-10-24 15:40:08 +0200

    init

---
+++ server-app-podiazd/src/main/groovy/com/example/serverapppodiazd/ServerAppPodiazdApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.serverapppodiazd
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ServerAppPodiazdApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ServerAppPodiazdApplication, args)
+	}
+
+}
---
+++ server-app-podiazd/src/test/groovy/com/example/serverapppodiazd/ServerAppPodiazdApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.serverapppodiazd
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerAppPodiazdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

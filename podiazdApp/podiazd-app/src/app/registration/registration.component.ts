b0d87aaa022e4946a10aaaf50cef0f321bcd882d
KarolinaHenig <karolina.henig@gmail.com>
2022-12-26 22:58:12 +0100

    login and registration working

--- podiazdApp/podiazd-app/src/app/registration/registration.component.ts
+++ podiazdApp/podiazd-app/src/app/registration/registration.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -13,13 +13,28 @@ export class RegistrationComponent implements OnInit {
   email = new FormControl('')
   name = new FormControl('')
   surname = new FormControl('')
-  password = new FormControl('', [Validators.required, Validators.minLength(this.minPw) ])
+  password = new FormControl('', [Validators.required, Validators.minLength(this.minPw)])
   hidePassword = true
-  repeatPassword = new FormControl('', [Validators.required, Validators.minLength(this.minPw) ])
+  repeatPassword = new FormControl('', [Validators.required, Validators.minLength(this.minPw)])
   hideRepeatPassword = true
 
-  constructor(private http: HttpClient,  private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {
+    this.repeatPassword.addValidators(
+      this.createCompareValidator(
+        this.password,
+        this.repeatPassword
+      )
+    )
+  }
 
+  createCompareValidator(controlOne: AbstractControl, controlTwo: AbstractControl) {
+    return () => {
+      if (controlOne.value !== controlTwo.value)
+        return { matchError: true }
+      return null
+    }
+
+  }
   register() {
     this.http.post('http://localhost:8080/api/v1/registration', {
       "name": this.name.value,
@@ -34,8 +49,12 @@ export class RegistrationComponent implements OnInit {
     console.log("register")
   }
 
+  onPassword() {
+    this.repeatPassword.updateValueAndValidity()
+  }
+
   ngOnInit(): void {
-    
+
   }
 
   getErrorMessageMail() {
@@ -50,11 +69,11 @@ export class RegistrationComponent implements OnInit {
   getErrorMessagePassword() {
     return "Please inform your password"
   }
-  getErrorMessageMinLenght(){
+  getErrorMessageMinLenght() {
     return "Passwords must be at least 8 characters"
   }
-  
-  getErrorPasswordMismatch(){
+
+  getErrorPasswordMismatch() {
     return "Passwords must be match"
   }
 }
